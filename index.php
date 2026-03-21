8f6e1eb0cd92eb5ae3f3edf99601db4ae215ada6
Mostafa Ahmed <momoformostafa@gmail.com>
2026-03-21 15:03:41 +0200

    1st-progress

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
+require "Database.php";
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+$config = require("config.php");
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = 'select * from posts where id = ?';
+
+$posts = $db->query($query, [$id])->fetchAll();
+
+dd($posts);
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
